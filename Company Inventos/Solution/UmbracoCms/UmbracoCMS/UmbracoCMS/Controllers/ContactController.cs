898f1007edbcb0103a48e27d904e2fe5f2eeb066
meyank <meyankpreet@gmail.com>
2017-09-07 00:23:08 +0530

    updated html and umbraco

---
+++ Company Inventos/Solution/UmbracoCms/UmbracoCMS/UmbracoCMS/Controllers/ContactController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace UmbracoCMS.Controllers
+{
+    public class ContactController : Umbraco.Web.Mvc.SurfaceController
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
